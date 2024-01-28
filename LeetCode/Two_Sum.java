5c20cbcbce58e4afddef602c2f01c512f70e8e30
jayant0709 <jayantpatil07092003@gamil.com>
2024-01-28 23:38:31 +0530

    Updated Two_Sum code.

--- LeetCode/Two_Sum.java
+++ LeetCode/Two_Sum.java
@@ -1,5 +1,30 @@
-public class test {
+import java.util.Arrays;
+
+public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{nums[left], nums[right]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[0];
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        System.out.println(Arrays.toString(result));
     }
 }
